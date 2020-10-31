11e6ddfa76dcf4a2bc4b745d0cd47ac12b0e6c7a
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-10-31 21:07:43 +0900

    外部スタイルシート読み込み

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,14 @@
 import Link from 'next/link';
-
-const h1 = {
-  fontSize: '68pt',
-  textAlign: 'right',
-  letterSpacing: '-8px',
-  color: '#f0f0f0',
-  margin: '-32px 0px',
-  fontWeight: 'bold',
-}
-
-const p = {
-  margin: '0px',
-  color: '#666',
-  fontSize:'16pt',
-}
+import style from '../static/style';
 
 export default () => <div>
-  <h1 style={h1}>Next.js</h1>
-  <p style={p}>Welcome to next.js!!</p>
+  {style}
+  <h1>Next.js</h1>
+  <p>Welcome to next.js!!</p>
   <hr />
   <div>
     <Link href="/other">
-      <a>Go to Other page &gt;&gt;</a>
+      <button>Go to Other page &gt;&gt;</button>
     </Link>
   </div>
 </div>
---
+++ static/style.js
@@ -0,0 +1,17 @@
+export default <style>{`
+  body {
+    margin: 10px;
+  }
+  h1 {
+    font-size: 68pt;
+    font-weight: bold;
+    letter-spacing: -8px;
+    color: #f0f0f0;
+    margin: -32px 0px;
+  }
+  p {
+    margin: 0px;
+    color: #666;
+    font-size: 16pt;
+  }
+`}</style>;
