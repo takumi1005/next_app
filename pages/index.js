90069fbfcc96d3f3a83c950c32c830b91a06c492
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-10-31 18:52:19 +0900

    ビルトインcss

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,20 @@
-const h1 = {
-  fontSize = '72pt',
-  fontWeight: 'bold',
-  textAlign: 'right',
-  letterSpacing: '-8px',
-  color: '#f0f0f0',
-  margin: '-40px 0px'
-}
-
-const p = {
-  margin: '0px',
-  color: '#666',
-  fontSize = '16pt',
-}
-
-export default <div>
-  <h1 style={h1}>Next.js</h1>
-  <p style={p}>Welcome to next.js!!</p>
+export default () => <div>
+  <style jsx> {`
+  h1 {
+    fontSize = '68pt',
+    fontWeight: 'bold',
+    textAlign: 'right',
+    letterSpacing: '-8px',
+    color: '#f0f0f0',
+    margin: '-32px 0px'
+  }
+  p {
+    margin: '0px',
+    color: '#666',
+    fontSize = '16pt',
+  }
+  `} </style>
+  <h1>Next.js</h1>
+  <p>Welcome to next.js!!</p>
+  <p>これはビルトインcssによるスタイルです</p>
 </div>
